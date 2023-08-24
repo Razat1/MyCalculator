059aeb334f9026f4988aac3ff0db2c6c34399d66
Razat Kaur <p2508373@my365.dmu.ac.uk>
2023-08-24 20:31:22 +0100

    calculator created that outputs for addition, subtraction, multiplication and division

---
+++ src/Calc.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Calc {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please enter your first number: ");
+        int num1 = userInput.nextInt();
+        System.out.println("Please enter your second number: ");
+        int num2 = userInput.nextInt();
+        //int add = num1 + num2;
+        int resultAdd = addition(num1, num2);
+        System.out.println("Your numbers added are " + num1 + " + " + num2 + " = " + resultAdd);
+        int resultSub = subtract(num1, num2);
+        System.out.println("Your numbers subtracted are " + num1 + " - " + num2 + " = " + resultSub);
+        int resultMult = multiplied(num1, num2);
+        System.out.println("Your numbers multiplied are " + num1 + " x " + num2 + " = " + resultMult);
+        int resultDiv = divided(num1, num2);
+        System.out.println("Your numbers divided are " + num1 + " / " + num2 + " = " + resultDiv);
+    }
+
+    public static int addition(int a, int b) {
+        int result = a + b;
+        return result;
+    }
+    public static int subtract(int a, int b){
+        int result = a - b;
+        return result;
+    }
+
+    public static int multiplied(int a, int b){
+        int result = a * b;
+        return result;
+    }
+
+    public static int divided(int a, int b){
+        int result = a / b;
+        return result;
+    }
+
+}
